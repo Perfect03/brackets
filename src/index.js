0990023c3ed4f844041dc86cd6912c27ca0ab19a
Perfect_03 <zlobin_gg52926@mail.ru>
2022-02-24 20:45:17 +0600

    feat: add index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,10 @@
-module.exports = function check(str, bracketsConfig) {
-  const OPEN_BRACKETS = ['(', '{'];
-  const BRACKETS_PAIR = {
-    [')']: '(',
-    ['}']: '{',
-  };
-  let stack = [];
-
-  for (let i = 0; i < str.length; i++) {
-    let currentSymbol = str[i];
-    
-    if (OPEN_BRACKETS.includes(currentSymbol)) {
-      stack.push(currentSymbol);
-    } else {
-      if (stack.length === 0) {
-        return false;
-      }
-
-      let topElement = stack[stack.length - 1];
-
-      if (BRACKETS_PAIR[currentSymbol] === topElement) {
-        stack.pop();
-      } else {
-        return false;
-      }
-    }
+module.exports = function check(str, bracket) {
+  let arr = [];
+for (let e of str) {
+    if (e != new Map(bracket).get(arr[arr.length - 1])) {
+      arr.push(e)
+    } else 
+    arr.pop(e)
   }
-
-  return true;
+  return arr.length === 0
 }
